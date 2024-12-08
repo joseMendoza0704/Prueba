f61524aaef5351a558fc6d4745aec9527c72d343
joseMendoza0704 <167268205+joseMendoza0704@users.noreply.github.com>
2024-12-07 21:06:45 -0600

    Prueba Pantalla

--- app/src/main/java/com/example/prueba/MainActivity.kt
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.prueba.Screens.HomeScreen
 
 import com.example.prueba.ui.theme.PruebaTheme
 
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
